76f7503e130c39f879f542d34d28d6b2b342c29e
Полина <polina@MacBook-Air-Polina.local>
2026-06-26 16:09:37 +0300

    feat: add rating colors

--- src/components/MovieCard/movieCard.tsx
+++ src/components/MovieCard/movieCard.tsx
@@ -4,12 +4,32 @@ import {format} from "date-fns";
 import { Rate } from 'antd';
 
 function MovieCard({genre_ids, overview, poster_path, release_date, title, vote_average}: ICard) {
+
+    function formatVotes(vote_average: number): string | null {
+        const vote = Number(vote_average.toFixed(1));
+        if (vote < 3) {
+            return styles.rating_red
+        }
+        if (vote >= 3 && vote <= 5) {
+            return styles.rating_orange
+        }
+        if (vote > 5 && vote <= 7) {
+            return styles.rating_yellow
+        }
+        if (vote > 7) {
+            return styles.rating_green
+        }
+        return null
+    }
+
     return (
         <div className={styles.card_container}>
             <img className={styles.card_poster} src={poster_path} alt="poster_path" />
             <div className={styles.card_description}>
                 <div className={styles.card_name}>{title}</div>
-                <div className={styles.card_rating}>{vote_average}</div>
+                <div className={styles.card_rating + " " + formatVotes(vote_average)}>
+                    {(Number(vote_average.toFixed(1)))}
+                </div>
                 <div className={styles.card_date}>{
                     release_date.length > 0 ? format(new Date(release_date), "MMMM dd, yyyy") : " "
                 }</div>
